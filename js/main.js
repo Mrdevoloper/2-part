f1c08b9304c0a7924d112e993b910653aadca4c9
Mrdevoloper <nasibullayevx@177gmail.com>
2021-11-25 19:36:41 -0700

    2-part

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+
+//     function info(evt) {
+//         var result = "";
+//         for (var i = 0; i < evt.length; i++) {   
+//             result = evt[i] + result;
+       
+//     }
+//     return result;   
+// }
+// console.log(info(" assalamu alaykum"));
+
+
+
+
+
+
+var inputText = prompt("Enter the words")
+
+function reverseString(string) {
+    
+    var split = string.split("");     
+    var revrse = split.reverse();   
+    var join = revrse.join("");  
+    return join; 
+}
+console.log(reverseString(inputText));
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+ 
+
